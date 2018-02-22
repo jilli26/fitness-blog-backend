0df75bd8c20f02f866cf8006be7cac7dd5565554
github username <github email address>
2018-02-22 16:52:47 -0500

    added neighborhood data

--- db/migrate/20180222202333_create_neighborhoods.rb
+++ db/migrate/20180222202333_create_neighborhoods.rb
@@ -2,8 +2,8 @@ class CreateNeighborhoods < ActiveRecord::Migration[5.1]
   def change
     create_table :neighborhoods do |t|
       t.string :name
-      t.integer :lat
-      t.integer :lng
+      t.decimal :lat
+      t.decimal :lng
 
       t.timestamps
     end
