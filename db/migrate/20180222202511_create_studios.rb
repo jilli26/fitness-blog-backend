a0e889bd3958b57e158206509f6844ef6cf5e447
github username <github email address>
2018-02-23 10:44:29 -0500

    created review seeds and updated review table

--- db/migrate/20180222202511_create_studios.rb
+++ db/migrate/20180222202511_create_studios.rb
@@ -11,6 +11,9 @@ class CreateStudios < ActiveRecord::Migration[5.1]
       t.string :instagram
       t.string :facebook
       t.string :image_url
+      t.integer :category_id
+      t.integer :neighborhood_id
+      t.integer :review_id 
 
       t.timestamps
     end
--- db/migrate/20180222202540_create_reviews.rb
+++ db/migrate/20180222202540_create_reviews.rb
@@ -2,6 +2,15 @@ class CreateReviews < ActiveRecord::Migration[5.1]
   def change
     create_table :reviews do |t|
       t.integer :rating
+      t.text :intensity
+      t.text :cardio
+      t.text :strength
+      t.text :knee_friendly
+      t.text :crowd
+      t.text :space
+      t.text :format
+      t.text :amenities
+      t.text :music 
 
       t.timestamps
     end
